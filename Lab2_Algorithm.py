3f87deda2d500222a62a2b9d3ca32c5fbe4580d8
danilfedorenko <94140635+danilfedorenko@users.noreply.github.com>
2024-05-06 23:26:15 +0300

    Lab2_Algorithm.py

--- Lab2_Algorithm.py
+++ Lab2_Algorithm.py
@@ -5,18 +5,20 @@ from numpy.linalg import multi_dot
 
 
 def generate_random_complex_matrix(size):
-    return np.random.rand(size, size) + 1j * np.random.rand(size, size)
+    real_part = np.random.rand(matrix_size, matrix_size).astype(np.float32)
+    imag_part = np.random.rand(matrix_size, matrix_size).astype(np.float32) * 1j
+    return (real_part + imag_part).astype(np.complex64)
 
 
-def matrix_multiply_algebr(A, B):
+def multiply_algebr(A, B):
     return np.dot(A, B)
 
 
-def matrix_multiply_cblas(A, B):
+def multiply_cblas(A, B):
     return scipy_blas.cgemm(1.0, A, B)
 
 
-def matrix_multiply_optimized(A, B):
+def multiply_matmul(A, B):
     # Перемножение матриц с использованием np.matmul
     return np.matmul(A, B)
 
@@ -28,6 +30,9 @@ def measure_performance(func, *args):
     execution_time = end_time - start_time
     return result, execution_time
 
+def compare_matrix(A, B, epsilon=1e-6):
+    return np.allclose(A, B, atol=epsilon)
+
 
 # Размер матрицы
 matrix_size = 1024
@@ -37,21 +42,33 @@ matrix_A = generate_random_complex_matrix(matrix_size)
 matrix_B = generate_random_complex_matrix(matrix_size)
 
 # Перемножение матриц по различным методам и измерение времени выполнения
-result_naive, time_naive = measure_performance(matrix_multiply_algebr, matrix_A, matrix_B)
-result_cblas, time_cblas = measure_performance(matrix_multiply_cblas, matrix_A, matrix_B)
-result_optimized, time_optimized = measure_performance(matrix_multiply_optimized, matrix_A, matrix_B)
+C1, time_algebr = measure_performance(multiply_algebr, matrix_A, matrix_B)
+C2, time_cblas = measure_performance(multiply_cblas, matrix_A, matrix_B)
+C3, time_matmul = measure_performance(multiply_matmul, matrix_A, matrix_B)
 
 # Оценка производительности в MFlops
 c = 2 * matrix_size ** 3
-mflops_algebr = c / (time_naive * 10 ** -6)
+mflops_algebr = c / (time_algebr * 10 ** -6)
 mflops_cblas = c / (time_cblas * 10 ** -6)
-mflops_optimized = c / (time_optimized * 10 ** -6)
+mflops_matmul = c / (time_matmul * 10 ** -6)
 
 # Вывод результатов
 print("Размерность матрицы:", matrix_size)
-print("Время выполнения (алгебраический метод):", time_naive, "сек")
+print(matrix_A[10][20])
+print(matrix_B[10][20])
+print("Время выполнения (алгебраический метод):", time_algebr, "сек")
 print("Время выполнения (cblas):", time_cblas, "сек")
-print("Время выполнения (оптимизированный метод):", time_optimized, "сек")
+print("Время выполнения (оптимизированный метод):", time_matmul, "сек")
 print("Производительность (алгебраический метод):", mflops_algebr, "MFlops")
 print("Производительность (cblas):", mflops_cblas, "MFlops")
-print("Производительность (оптимизированный метод):", mflops_optimized, "MFlops")
+print("Производительность (оптимизированный метод):", mflops_matmul, "MFlops")
+
+if compare_matrix(C1, C2):
+    print("Матрицы C1 и C2 равны.")
+else:
+    print("Матрицы C1 и C2 не равны.")
+
+if compare_matrix(C1, C3):
+    print("Матрицы C1 и C3 равны.")
+else:
+    print("Матрицы C1 и C3 не равны.")
